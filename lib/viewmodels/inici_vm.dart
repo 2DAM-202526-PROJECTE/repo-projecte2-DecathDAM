73fbae727b596a5ca5bf3ed6e5418addd58dab40
«acaballee» <alexcaballe@iesebre.com>
2026-01-15 19:50:22 +0100

    creacio i rectificacio creaproductes

---
+++ lib/viewmodels/inici_vm.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'craft.dart'; 
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Center(
+      child: Text(
+        'Inici',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Cerca',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Afegir',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Notificacions',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Perfil',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DecathDAM'),
+        // Using Theme.of(context) which will rely on main.dart theme, or we can hardcode if needed
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Inici'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Cerca'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_outline),
+            label: 'Afegir',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Avisos',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Perfil'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
